bc395216be206bb7763a9012234c62c6e16cc03b
Dmirty <9048881951@mail.ru>
2022-08-07 21:02:33 +0500

    1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// #1
+/*
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit2 = (num / 10) % 10;
+Console.WriteLine(digit2);
+*/
+
+// #2
+/*
+Console.WriteLine("Введите число: ");
+string? num = Console.ReadLine();
+if ((num.Length) < 3) { Console.WriteLine("третьей цифры нет"); }
+else {Console.WriteLine("Третья цифра числа: " + num[2]);}
+*/
+
+// #3
+/*
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7) { Console.WriteLine("Да"); }
+else { Console.WriteLine("Нет"); }
+*/
